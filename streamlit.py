9350dfe19d284faa34196e7e600fe438e4bb2cd9
Vivek Gautam <vivek.gautam2022@vitstudent.ac.in>
2025-04-18 22:57:26 +0530

    add

--- streamlit.py
+++ streamlit.py
@@ -4,3 +4,34 @@ import streamlit as st
 
 st.title("Streamlit App")
 st.write("This is a simple Streamlit app.")
+
+
+#header
+
+st.header("Header")
+st.write("This is a header.")
+
+#subheader  
+st.subheader("Subheader")
+st.write("This is a subheader.")
+
+st.info("Hey brother")
+
+st.warning("yops")
+
+st.success("Succhhess")
+st.write(range(50))
+
+
+#error msg
+
+st.error("Error")
+
+
+st.markdown("**bold**")
+st.markdown("### bold")
+
+st.markdown(":moon:")
+
+#to write caption
+
